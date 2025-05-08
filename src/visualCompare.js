c257d280c46da5f404c54409b2d0dff4b849ddb8
gauravawale <gauravawale1990@gmail.com>
2025-05-08 11:10:48 +0530

    [GA]test- 2

--- src/visualCompare.js
+++ src/visualCompare.js
@@ -77,7 +77,7 @@ class VisualCompare {
 
     async _setupPage() {
         await this.browser.launch();
-        const page = page.goto(this.url1).catch(err => console.error('goto failed:', err));
+        const page = this.browser.goto(this.url1).catch(err => console.error('goto failed:', err));
 
         await this.browser.setViewport(page, this.viewport.width, this.viewport.height);
 
